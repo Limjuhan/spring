6d98997044bfc2ba746d6ace148bf54921c01d8f
Limjuhan <starlexy@naver.com>
2025-03-11 21:00:08 +0900

    start

--- spinrgmvc/src/main/java/hello/spinrgmvc/basic/response/ResponseBodyController.java
+++ spinrgmvc/src/main/java/hello/spinrgmvc/basic/response/ResponseBodyController.java
@@ -52,7 +52,7 @@ public class ResponseBodyController {
 
     @ResponseStatus(HttpStatus.OK)
 //    @ResponseBody
-    @GetMapping("/response-body-json-v1")
+    @GetMapping("/response-body-json-v2")
     public HelloData responseBodyJsonV2() {
         HelloData helloData = new HelloData();
         helloData.setUsername("userA");
