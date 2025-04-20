1227c378d7b6c5d09dec43f77055b6506d5f89cb
Limjuhan <starlexy@naver.com>
2025-04-20 10:38:12 +0900

    타입컨버터 끝

--- typeconverter/src/main/java/hello/typeconverter/type/IpPort.java
+++ typeconverter/src/main/java/hello/typeconverter/type/IpPort.java
@@ -2,9 +2,11 @@ package hello.typeconverter.type;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 
 
 @Getter
+@Setter
 @EqualsAndHashCode
 public class IpPort {
 
