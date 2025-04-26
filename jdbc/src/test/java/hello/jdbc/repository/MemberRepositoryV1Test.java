6d3956bdcb12535cb0021d8e21bd70bcb60fcb87
Limjuhan <starlexy@naver.com>
2025-04-26 22:37:24 +0900

    DB컨넥션 풀,락

---
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,63 @@
+package hello.jdbc.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MemberRepositoryV1Test {
+    private static final Logger log = LoggerFactory.getLogger(MemberRepositoryV1Test.class);
+    MemberRepositoryV1 memberRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        // 기본 DriverManager - 항상 새로운 커넥션을 획득 고로 성능이 느림
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        memberRepository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+        // save
+        Member member = new Member("memberV100", 10000 );
+        memberRepository.save(member);
+
+        // findById
+        Member findMember = memberRepository.findById(member.getMemberId());
+        log.info("Find member: {}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        //update: money:10000->20000
+        memberRepository.update(member.getMemberId(), 20000);
+        Member updatedMember = memberRepository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        memberRepository.delete(member.getMemberId());
+        assertThatThrownBy(() -> memberRepository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
